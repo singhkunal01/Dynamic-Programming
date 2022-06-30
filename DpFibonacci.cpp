0526c24c86709e58b99a576234f73b76cdf5a484
Kunal Singh <kunalsingh142001@gmail.com>
2022-06-30 23:55:43 +0530

    First Program added

--- DpFibonacci.cpp
+++ DpFibonacci.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int mod = 1e9 + 7;
 //first program using dp of fibonacci series to give the nth fibonacci number
 
-int giveNthFibonacci(int n) {
+int giveNthFibonacci(int n, vector<long long> &dp) {
 	if (n <= 1) return n;// base case for the first two numbers which are fixed as 0 and 1.
-	long dp[n + 1] = { -1}; //dp array which holds the overlappings
-	if (dp[n] == -1)return dp[n];
-	return dp[n] = giveNthFibonacci(n - 1) + giveNthFibonacci(n - 2);
+	if (dp[n] != -1)return dp[n];
+	return dp[n] = giveNthFibonacci(n - 1, dp) % mod + giveNthFibonacci(n - 2, dp) % mod;
 }
 
 //main function which holds the answer
@@ -15,7 +15,9 @@ int giveNthFibonacci(int n) {
 int main() {
 	int num;
 	cin >> num;
+	vector<long long> dp(num + 1, -1);
 	for (int i = 0; i <= num; i++)
-		cout << giveNthFibonacci(i) << " ";
+		cout << giveNthFibonacci(i, dp) << " ";
+	return 0;
 }
 
