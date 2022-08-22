56753e863cc3354a79a975bc964c9613bc9ad126
Kunal Singh <kunalsingh142001@gmail.com>
2022-08-22 13:39:07 +0530

    Added the space optimise and tabulation approach for Rod Cutting Problem.cpp

--- DP on Subsequences/Rod Cutting Problem.cpp
+++ DP on Subsequences/Rod Cutting Problem.cpp
@@ -19,9 +19,54 @@ int waysToCutRod(int part, vector<int> &price, vector<vector<int>> &memo, int cu
 	return memo[part][currLength] = max(cut, notCut);
 }
 
-int cutRod(vector<int> &price, int n) {
+
+/*tabulation approach */
+int waysToCutRodTabul(vector<int> &price, int n) {
 	vector<vector<int>> memo(n, vector<int> (n + 1, -1));
-	int res = waysToCutRod(n - 1, price, memo, n);
+//for the base case
+	for (int i = 0; i <= n; i++)memo[0][i] = i * price[0];
+	for (int part = 1; part < n; part++) {
+		for (int currLength = 0; currLength <= n; currLength++) {
+			int notCut = memo[part - 1][currLength];
+			int cut = INT_MIN;
+			int currRodLength = part + 1;
+			if (currRodLength <= currLength)cut = price[part] + memo[part][currLength - currRodLength];
+			memo[part][currLength] = max(cut, notCut);
+		}
+	}
+	return memo[n - 1][n];
+}
+
+
+/*space optimisation approach */
+int waysToCutRodSpaceOpt(vector<int> &price, int n) {
+	vector<int> prev(n + 1), curr(n + 1);
+//for the base case
+	for (int i = 0; i <= n; i++)prev[i] = i * price[0];
+	for (int part = 1; part < n; part++) {
+		for (int currLength = 0; currLength <= n; currLength++) {
+			int notCut = prev[currLength];
+			int cut = INT_MIN;
+			int currRodLength = part + 1;
+			if (currRodLength <= currLength)cut = price[part] + curr[currLength - currRodLength];
+			curr[currLength] = max(cut, notCut);
+		}
+		prev = curr;
+	}
+	return prev[n];
+}
+
+int cutRod(vector<int> &price, int n) {
+
+	//for memoization -
+	// vector<vector<int>> memo(n, vector<int> (n + 1, -1));
+	// int res = waysToCutRod(n - 1, price, memo, n);
+
+	//for tabulation
+	// int res = waysToCutRodTabul(price, n);
+
+	//for space optimisation
+	int res = waysToCutRodSpaceOpt(price, n);
 	return res;
 }
 
