d1786bd1a66887778c2702933486e6ced0e4dde1
Kunal Singh <kunalsingh142001@gmail.com>
2022-08-28 23:46:43 +0530

    Enhanced the naming of parameters

--- DP on Strings/Wildcard Matching.cpp
+++ DP on Strings/Wildcard Matching.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool wildcardMatch(int i, int j, string &s, string &t, vector<vector<int>>&dp) {
+bool wildcardMatch(int i, int j, string &pattern, string &text, vector<vector<int>>&dp) {
 
 //base cases
 //1 . if both the strings exhausted
@@ -13,32 +13,36 @@ bool wildcardMatch(int i, int j, string &s, string &t, vector<vector<int>>&dp) {
 	so whatever string has ? and * if string other than that is empty then there have to only *
 	in the another string because only * will match empty characters so*/
 	if (j<0 and i >= 0) {
-		for (int k = 0; k <= i; k++ )if (s[k] != '*')
+		for (int k = 0; k <= i; k++ )if (pattern[k] != '*')
 				return false;
 		return true;
 	}
 	if (dp[i][j] != -1) return dp[i][j];
 //now main logic
 // if both the char matched or a single character is matched with ? then move both pointers
-	if (s[i] == t[j] or s[i] == '?') return dp[i][j] = wildcardMatch(i - 1, j - 1, s, t, dp);
-	if (s[i] == '*') return dp[i][j] = (wildcardMatch(i - 1, j, s, t, dp) | wildcardMatch(i, j - 1, s, t, dp));
+	if (pattern[i] == text[j] or pattern[i] == '?') return dp[i][j] = wildcardMatch(i - 1, j - 1, pattern, text, dp);
+	if (pattern[i] == '*') return dp[i][j] = (wildcardMatch(i - 1, j, pattern, text, dp) | wildcardMatch(i, j - 1, pattern, text, dp));
 	return dp[i][j] = false;
 
 }
 
-bool isMatch(string &s, string &t) {
-	int n = s.size(), m = t.size();
+/*tabulation code - */
+
+
+
+bool isMatch(string &pattern, string &text) {
+	int n = pattern.size(), m = text.size();
 
 	//for memoization
 	vector<vector<int>> dp(n, vector<int>(m, -1));
-	bool res = wildcardMatch(n - 1, m - 1, s, t, dp);
+	bool res = wildcardMatch(n - 1, m - 1, pattern, text, dp);
 	return res;
 }
 
 int main() {
-	string s, t;
-	cin >> s >> t;
-	cout << isMatch(s, t);
+	string pattern, text;
+	cin >> pattern >> text;
+	cout << isMatch(pattern, text);
 	return 0;
 
 }
