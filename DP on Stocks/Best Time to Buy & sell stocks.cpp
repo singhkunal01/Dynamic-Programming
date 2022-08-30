92363008df84bb18235bf571e11d1e89cbcb096d
Kunal Singh <kunalsingh142001@gmail.com>
2022-08-31 01:54:59 +0530

    Added Best Time to Buy & sell stocks.cpp

---
+++ DP on Stocks/Best Time to Buy & sell stocks.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxProfit(vector<int>& prices) {
+	int n = prices.size();
+	int buyAt = prices[0], profitOf = 0;
+	for (int i = 1 ; i < n; i++) {
+		int cost = prices[i] - buyAt; //total profit or loss
+		profitOf = max(profitOf, cost);
+		buyAt = min(buyAt, prices[i]);
+	}
+	return profitOf;
+}
+
+int main() {
+	int days;
+	cin >> days;
+	vector<int> prices(days);
+	for (int &pricePerDay : prices) cin >> pricePerDay;
+	cout << maxProfit(prices);
+	return 0;
+
+}
+
+//singhkunal01
