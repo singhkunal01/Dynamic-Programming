7da8c4bb5ae1ee290c2bd13595585faac44413b2
Kunal Singh <kunalsingh142001@gmail.com>
2022-09-05 14:17:28 +0530

    Added the space optimisation approach for the Best Time to Buy & Sell stock with Cooldown.cpp

--- DP on Stocks/Best Time to Buy & Sell stock with Cooldown.cpp
+++ DP on Stocks/Best Time to Buy & Sell stock with Cooldown.cpp
@@ -22,38 +22,48 @@ int findMaxProfitCooldown(int i, int n, vector<int> &profit, int buy, int prof, 
 int findMaxProfitCooldownTabul(vector<int> &profit) {
 	int n = profit.size();
 	vector<vector<int>> dp(n + 2, vector<int> (2, 0));//2 because sometimes we gonna jump 2 times because of that sometimes we go beyond the n
-	dp[n][0] = dp[n][1] = 0;
+	dp[n][0] = dp[n][1] = 0;//no need to add this base although
 
 //bottom up because in memoization we start from the  0 so here we do invert
 	for (int i = n - 1; i >= 0; i--) {
-		for (int buy  = 0; buy <= 1; buy++) {
-			long long prof = 0;
-			if (buy) {
-				dp[i][buy] = max(-profit[i] + dp[i + 1][0], dp[i + 1][1]);
-			}
-			else {
-				dp[i][buy] = max(profit[i] + dp[i + 2][1], dp[i + 1][0]);
-
-			}
-		}
+		dp[i][1] = max(-profit[i] + dp[i + 1][0], dp[i + 1][1]);
+		dp[i][0] = max(profit[i] + dp[i + 2][1], dp[i + 1][0]);
 	}
 	return dp[0][1];
-
 }
 
 
 /* space optimisation - We can space optimise it but because as we do i + 2 which means we also need prev of prev because of
-that we have to maintain 3 arrays for getting the i + 2 so we don't optimise it .*/
+that we have to maintain 3 arrays for getting the i + 2 so this is the solution for that */
+
+int findMaxProfitCooldownSpaceOpt(vector<int> &profit) {
+	int n = profit.size();
+	vector<int> prev(2, 0), prevOfprev(2, 0), curr(2, 0);
+	//size 2 because sometimes we gonna jump 2 times because of that sometimes we go beyond the n and arrays 3 because of i+2
+
+//bottom up because in memoization we start from the  0 so here we do invert
+	for (int i = n - 1; i >= 0; i--) {
+		curr[1] = max(-profit[i] + prev[0], prev[1]);
+		curr[0] = max(profit[i] + prevOfprev[1], prev[0]);
+		prevOfprev = prev;
+		prev = curr;
+	}
+	return prev[1];
+}
+
+
 
 int maxProfit(vector<int> &profit) {
 	int n = profit.size();
-//for memoization
-// vector<vector<int>> dp(n, vector<int> (2, -1));
-// int res = findMaxProfitCooldown(0, n, profit, 1, 0, dp);
+	//for memoization
+	// vector<vector<int>> dp(n, vector<int> (2, -1));
+	// int res = findMaxProfitCooldown(0, n, profit, 1, 0, dp);
 
-//for tabulation
-	int res = findMaxProfitCooldownTabul(profit);
+	//for tabulation
+	// int res = findMaxProfitCooldownTabul(profit);
 
+//for space optimisation
+	int res = findMaxProfitCooldownSpaceOpt(profit);
 	return res;
 
 }
@@ -68,3 +78,9 @@ int main() {
 	return 0;
 
 }
+
+/*Note :
+- We can omit the inner buy loop because whenever the buy is 1 then only that buy function called else the sell
+function called so by removing it just place the buy[0] for the buy and buy[1] for the sell.
+
+*/
