eba003f53f1ff96c32be6016873c9392ce079971
Kunal Singh <kunalsingh142001@gmail.com>
2022-09-13 16:21:26 +0530

    Added the tabulation code for Minimum cost to cut the Stick.cpp

--- DP On Partitions(MCM) Pattern/Minimum cost to cut the Stick.cpp
+++ DP On Partitions(MCM) Pattern/Minimum cost to cut the Stick.cpp
@@ -2,26 +2,50 @@
 using namespace std;
 
 //memoisation code
-
 long giveCost(int i, int j, vector<int>&cuts, vector<vector<int>> &dp) {
 	if (i > j) return 0;
 	if (dp[i][j] != -1) return dp[i][j];
-	long minCost = INT_MAX;
+	long miniCost = INT_MAX;
 	for (int k = i; k <= j; k++) {
 		long cost = cuts[j + 1] - cuts[i - 1] + giveCost(i, k - 1, cuts, dp) + giveCost(k + 1, j, cuts, dp);
-		minCost = min(minCost, cost);
+		miniCost = min(miniCost, cost);
 	}
-	return dp[i][j] = minCost;
+	return dp[i][j] = miniCost;
 }
+
+//tabulation code
+long giveCostTabul(int m, vector<int> &cuts) {
+	vector<vector<int>> dp(m + 2, vector<int> (m + 2, 0));
+	for (int i = m; i > 0; i--) {
+		for (int j = 1; j <= m; j++) {
+			if (i > j) continue;
+			long miniCost = (int)INT_MAX;
+			for (int ind = i; ind <= j; ind++) {
+				long cost = cuts[j + 1] - cuts[i - 1] + dp[i][ind - 1] + dp[ind + 1][j];
+				miniCost = min(miniCost, cost);
+			}
+			dp[i][j] = miniCost;
+		}
+	}
+	return dp[1][m];
+}
+
 
 //main code
 int minCost(int n, vector<int>& cuts) {
 	int m = cuts.size();
-	cuts.insert(cuts.begin(), 0);
+	cuts.insert(cuts.begin(), 0);//insert the 0 at starting
 	cuts.push_back(n);
 	sort(begin(cuts), end(cuts));
-	vector<vector<int>> dp(m + 1, vector<int> (m + 1, -1));
-	return giveCost(1, m, cuts, dp);
+
+	// for memoisation
+	// vector<vector<int>> dp(m + 1, vector<int> (m + 1, -1));
+	// int res =  giveCost(1, m, cuts, dp);
+
+
+	//for tabulation
+	int res = giveCostTabul(m, cuts);
+	return res;
 }
 
 
