2f6930dee525f62d56ae6b6d945d0a3601b54cc3
Kunal Singh <kunalsingh142001@gmail.com>
2022-09-28 12:38:27 +0530

    Added the tabulation code for the Boolean Evaluation.cpp

--- DP On Partitions(MCM) Pattern/Boolean Evaluation.cpp
+++ DP On Partitions(MCM) Pattern/Boolean Evaluation.cpp
@@ -40,10 +40,61 @@ long long findWays(int i, int j, int isTrue, string &exp, vector<vector<vector<l
 }
 
 
+/*tabulation code */
+int findWaysTabul(int n, string &exp) {
+	vector<vector<vector<long long>>> dp(n , vector<vector<long long>>(n , vector<long long>(2, -1)));
+	for (int i = 0; i < n; i++) {
+		dp[i][i][1] = exp[i] == 'T';
+		dp[i][i][0] = exp[i] == 'F';
+	}
+
+//rest of all stuff
+	for (int i = n - 1; i >= 0; i--) {
+		for (int j = 0; j < n; j++) {
+			for (int isTrue = 0; isTrue <= 1; isTrue++) {
+				if (i == j) continue;
+				if (i > j) dp[i][j][isTrue] = 0;
+				long long ways = 0;
+				for (int ind = i + 1; ind <= j - 1; ind += 2) {
+					int leftTrue = dp[i][ind - 1][ 1];
+					int leftFalse = dp[i][ ind - 1][ 0];
+					int rightTrue = dp[ind + 1][ j][ 1];
+
+					int rightFalse = dp[ind + 1][ j][ 0];
+					//if the operator is &
+					if (exp[ind] == '&') {
+						if (isTrue) ways = (ways + (leftTrue * rightTrue) % mod) % mod;
+						else ways = (ways + (leftTrue * rightFalse) % mod + (leftFalse * rightTrue) % mod + (leftFalse * rightFalse) % mod) % mod;
+					}
+					//if the operator is |
+					if (exp[ind] == '|') {
+						if (isTrue) ways = (ways + (leftTrue * rightTrue) % mod + (leftTrue * rightFalse) % mod + (leftFalse * rightTrue) % mod) % mod ;
+						else ways = (ways + (leftFalse * rightFalse) % mod) % mod;
+					}
+					//if the operator is ^
+					if (exp[ind] == '^') {
+						if (isTrue) ways = (ways +  (leftTrue * rightFalse ) % mod + (leftFalse * rightTrue ) % mod) % mod;
+						else ways = (ways + (leftTrue * rightTrue) % mod + (leftFalse * rightFalse) % mod) % mod;
+					}
+				}
+				dp[i][j][isTrue] = ways % mod;
+			}
+		}
+	}
+	return dp[0][n - 1][1];
+}
+
+
 int countWays(int n, string exp) {
+
+	/*memoisation code*/
 	vector<vector<vector<long long>>> dp(n , vector<vector<long long>>(n , vector<long long>(2, -1)));
 	int  isTrue = 1;
-	int res = findWays(0, n - 1, 1, exp, dp);
+	// int res = findWays(0, n - 1, 1, exp, dp);
+
+
+	//tabulation code
+	int res = findWaysTabul(n, exp);
 	return res;
 }
 
